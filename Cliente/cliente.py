ebe928c1fb16be4935fa1ef347ec9cd67ea80dd8
bravo95 <andresfelipe.bravo@utp.edu.co>
2018-11-19 00:15:36 -0500

    modificacion y actualizacion de funciones, falta comunicacion con servidores finales

--- Cliente/cliente.py
+++ Cliente/cliente.py
@@ -1,15 +1,33 @@
 import socket
 import sys
 
+### //crear coneccion
 s = socket.socket()
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.connect(('localhost', 9999))
+###
+def pedirIP(self):
+	client_socket . send ("get ips")
 
-cpu=raw_input("\ningrese el porcentaje de cpu a utilizar: ")
-memoria=raw_input("\ningrese el porcentaje de memoria a utilizar: ")
 
-mensaje=cpu+" "+memoria
+#direccion = client_socket.recv(1024)
+def recibeDirecciones(self,cadena):
+	direcciones = cadena
+	direcciones.split(",")
+	return direcciones
 
-client_socket . send (n1 + " " + n2 + " " + n3)
-direccion = client_socket.recv(1024)
+def recorrerDirecciones(self,listaDireccion):
+	direcciones = []
+	direcciones = listaDireccion
+	while ("respuesta sea 0"):
+		#hacer split por espacio de Ip y puerto
+		#hacer coneccion al servidor y esperar respuesta
+
+def pedirDatos(self):
+	cpu=raw_input("\ningrese el porcentaje de cpu a utilizar: ")
+	memoria=raw_input("\ningrese el porcentaje de memoria a utilizar: ")
+	mensaje=cpu+" "+memoria
+	return mensaje
+
+client_socket . send (mensaje)
 
