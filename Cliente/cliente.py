ef91051bdafc7da2f2281b7190576173adc38bf5
bravo95 <andresfelipe.bravo@utp.edu.co>
2018-11-19 11:39:38 -0500

    configuracion y modo de envio y recepcion de la informacion

--- Cliente/cliente.py
+++ Cliente/cliente.py
@@ -5,7 +5,7 @@ import sys
 s = socket.socket()
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.connect(('localhost', 9999))
-###
+### Envia un mensaje solicitando las Ip en un string por parte del servidor de distribucion
 def pedirIP(self):
 	client_socket . send ("get ips")
 
@@ -16,12 +16,6 @@ def recibeDirecciones(self,cadena):
 	direcciones.split(",")
 	return direcciones
 
-def recorrerDirecciones(self,listaDireccion):
-	direcciones = []
-	direcciones = listaDireccion
-	while ("respuesta sea 0"):
-		#hacer split por espacio de Ip y puerto
-		#hacer coneccion al servidor y esperar respuesta
 
 def pedirDatos(self):
 	cpu=raw_input("\ningrese el porcentaje de cpu a utilizar: ")
@@ -29,5 +23,28 @@ def pedirDatos(self):
 	mensaje=cpu+" "+memoria
 	return mensaje
 
-client_socket . send (mensaje)
+def recorrerDirecciones(self,listaDireccion):
+	direcciones = []
+	direcciones = listaDireccion
+	mensaje = pedirDatos()
+	respuesta = 0
+	cont = 0
+	while (respuesta == 0):
+		#hacer split por espacio de Ip y puerto
+		ip,port = direcciones[cont].split(" ")
+		s1 = socket.socket()
+		s1.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		s1.connect((ip, port))
+		client_socket . send (mensaje)
+		#hacer coneccion al servidor y esperar respuesta
+
+
+
+client_socket . close ()
+
+if __name__ == '__main__':
+	pedirIP() # envia peticion de ip a servidor de distribucion
+	direccion = client_socket.recv(1024) # recibe un string con todas las direcciones por parte del servidor de distribucion
+	lista = recibeDirecciones(direccion) #realiza la separacion de todas las direcciones y las devuelve en una lista
+	recorrerDirecciones(lista)
 
