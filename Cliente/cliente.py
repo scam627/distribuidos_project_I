e560b693260677dd8a881f7aab363c4bdeed23cf
bravo95 <andresfelipe.bravo@utp.edu.co>
2018-11-21 17:32:16 -0500

    se termina cliente mostrando en pantalla si ejecuta o no el proceso y en caso de ejecutarlo muestra la direccion donde lo ejecuta, se crean servidores de distribucion y servidores de trabajo que de manera abstracta devuelven un supuesto de lo que se esta realizando alli

--- Cliente/cliente.py
+++ Cliente/cliente.py
@@ -2,30 +2,31 @@ import socket
 import sys
 
 ### //crear coneccion
-s = socket.socket()
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.connect(('localhost', 9999))
+sDist = socket.socket()
+sDist.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sDist.connect(('localhost', 9997))
 ### Envia un mensaje solicitando las Ip en un string por parte del servidor de distribucion
-def pedirIP(self):
-	client_socket . send ("get ips")
+def pedirIP():
+	sDist . send ("get ips")
 
 
 #direccion = client_socket.recv(1024)
-def recibeDirecciones(self,cadena):
-	direcciones = cadena
-	direcciones.split(",")
+def recibeDirecciones(cadena):
+	#direcciones = cadena
+	direcciones = cadena.split(",")
 	return direcciones
 
 
-def pedirDatos(self):
+def pedirDatos():
 	cpu=raw_input("\ningrese el porcentaje de cpu a utilizar: ")
 	memoria=raw_input("\ningrese el porcentaje de memoria a utilizar: ")
 	mensaje=cpu+" "+memoria
 	return mensaje
 
-def recorrerDirecciones(self,listaDireccion):
+def recorrerDirecciones(listaDireccion):#retorna si se ejecuta el proceso o no, en caso de si, muestra donde.
 	direcciones = []
 	direcciones = listaDireccion
+	#print type(direcciones[0])
 	mensaje = pedirDatos()
 	respuesta = '0'
 	estado = "no se cumplio"
@@ -33,17 +34,19 @@ def recorrerDirecciones(self,listaDireccion):
 	for l in direcciones:
 	#while (respuesta == '0'):
 		#hacer split por espacio de Ip y puerto
-		ip,port = direcciones[l].split(" ")
+		ip,port = l.split()
 		s1 = socket.socket()
 		s1.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		s1.connect((ip, port))
-		client_socket . send (mensaje)
-		respuesta = client_socket.recv(1024) # respuesta???
+		s1.connect((ip, int(port)))
+		s1 . send (mensaje)
+		respuesta = s1.recv(1024) # respuesta???
 		#hacer coneccion al servidor y esperar respuesta
 		if (respuesta == '1'):
 			estado = "se cumplio"
+			print "servicio ejecutado en: ",ip,port
 			return estado
 		#cont += 1
+		s1.close()
 	return estado
 
 
@@ -52,12 +55,16 @@ def recorrerDirecciones(self,listaDireccion):
 
 if __name__ == '__main__':
 	pedirIP() # envia peticion de ip a servidor de distribucion
-	direccion = client_socket.recv(1024) # recibe un string con todas las direcciones por parte del servidor de distribucion
-	client_socket . close ()
+	direccion = sDist.recv(1024) # recibe un string con todas las direcciones por parte del servidor de distribucion
+	sDist . close ()#funciona
+	
 	lista = recibeDirecciones(direccion) #realiza la separacion de todas las direcciones y las devuelve en una lista
+	#print lista[1]
+	
 	mostrar = recorrerDirecciones(lista)
 	if (mostrar == '0'):
 		pass
 		pass
 		mostrar = recorrerDirecciones(lista)
 	print mostrar
+	
---
+++ Cliente/servidor2.py
@@ -0,0 +1,38 @@
+import socket
+import sys
+import thread
+import math
+ 
+HOST = ''   # Symbolic name, meaning all available interfaces
+PORT = 9991 # Arbitrary non-privileged port
+ 
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print 'Socket created'
+
+#Bind socket to local host and port
+try:
+    serversocket.bind((HOST, PORT))
+except socket.error as msg:
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+     
+print 'Socket bind complete'
+
+#Start listening on socket
+serversocket.listen(10)
+print 'Socket now listening'
+
+def handler(conn, addr):    
+	data = conn . recv (1025)
+	reply = "1"
+	conn.sendall(reply)
+
+#now keep talking with the client
+while 1:
+    #wait to accept a connection - blocking call
+    conn, addr = serversocket.accept()    
+    print 'Connected with ' + addr[0] + ':' + str(addr[1])
+    thread . start_new_thread (handler, (conn, addr))
+    
+     
+s.close()
